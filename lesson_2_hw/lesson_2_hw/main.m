03001423ed440a9eeae1ed84944a34271e4aebf4
maxud <mlenon13@gmail.com>
2017-08-14 09:28:55 +0300

    добавили проект

---
+++ lesson_2_hw/lesson_2_hw/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  lesson_2_hw
+//
+//  Created by maxud on 14.08.17.
+//  Copyright © 2017 lesson_1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
