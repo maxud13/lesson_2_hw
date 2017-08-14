0c7b62f016e8ef3cbe58abdbc4b77c4f165cc88e
maxud <mlenon13@gmail.com>
2017-08-14 09:55:11 +0300

    переменные

--- lesson_2_hw/lesson_2_hw/main.m
+++ lesson_2_hw/lesson_2_hw/main.m
@@ -9,9 +9,25 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+    @autoreleasepool
+    {
+        BOOL variableBool=YES;
+        variableBool=NO;
+
+        NSInteger variableinteger=13;
+        variableinteger=-13;
+
+        CGFloat varieblefloat=13.1;
+        varieblefloat=-13.1;
+
+        NSNumber *variablenumber=@(variableinteger);
+        variablenumber=@(varieblefloat);
+        variablenumber=@(variableBool);
+        
+        NSString *variablestring=@"text";
+        variablestring=@"text2";
+
+        NSArray *variableArray = @[@(variableBool), @(variableinteger), @(varieblefloat),  variablenumber, variablestring];
     }
-    return 0;
+    return 0
 }
