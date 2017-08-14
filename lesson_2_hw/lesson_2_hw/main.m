ecb3babad8308457af84711bf70bcc26ca5b2baa
maxud <mlenon13@gmail.com>
2017-08-14 18:53:26 +0300

    доделать

--- lesson_2_hw/lesson_2_hw/main.m
+++ lesson_2_hw/lesson_2_hw/main.m
@@ -10,8 +10,34 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        CGFloat priceofLatte=22.5;
+        CGFloat priceofcappucinno=20.5;
+        CGFloat priceofEspresso=15.5;
+        CGFloat priceofamericano=23.5;
+        
+        NSInteger currentClientsCount=15;
+        NSInteger oneHourAgoClientsCount=23;
+        
+        NSInteger latteSoldCount  = 4;
+        NSInteger cappuccinoSoldCount = 5;
+        NSInteger espressoSoldCount = 3;
+        NSInteger americanoSoldCount = 7;
+        
+        cappuccinoSoldCount =+3;
+        americanoSoldCount =+4;
+        currentClientsCount=-3;
+        
+        BOOL even;
+        
+        if(currentClientsCount%2==0)
+        {
+            even = true;
+        }
+        else even = false;
+        NSLog ("%d",latteSoldCount);
+        
     }
     return 0;
 }
+
+    
