5e0956970cb7cf883edd169b045c48694ba0204d
maxud <mlenon13@gmail.com>
2017-08-15 18:33:47 +0300

    циклы

--- lesson_2_hw/lesson_2_hw/main.m
+++ lesson_2_hw/lesson_2_hw/main.m
@@ -11,7 +11,40 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
-    }
+        NSInteger i = 0;
+        for (NSInteger i = 0; i < 21; i++)
+            if (i % 5==0)
+            {
+                NSLog(@"число кратно 5");
+            
+
+        
+            NSLog(@"Индекс i == %lu, строка: %lu", i, i+1);
+        }
+        if (i % 5==0)
+        {
+            NSLog(@"число кратно 5");
+        }
+        NSInteger count = 0;
+        
+        do
+        {
+            NSLog(@"count = %lu", count);
+            count += 7; // count = count + 7;
+        }
+        while (count < 50);
+        
+        count = 0;
+        while (count<100)
+        {
+            NSLog(@"count = %lu", count);
+            count+=3;
+            
+            if (count > 30)
+            {
+                break;
+            }
+        }
+}
     return 0;
 }
